6b4a546f1ffd360bb2c3c0abf640285245bf1a0f
iam_Vaibhav <vaibhavthobbi93@gmail.com>
2023-04-05 08:39:04 +0530

    updatingcontext

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -8,9 +8,23 @@ class ThemeContextProvider extends Component {
     light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
     dark: { syntax: "#ddd", ui: "#333", bg: "#555" },
   };
+  toggleTheme = () => {
+    this.setState({ isLightTheme: !this.state.isLightTheme });
+  };
+  //Due to the async nature of setState,
+  // it is not advisable to use this.state to get the previous state within setState.
+  // Both prevState and props received by the updater function are guaranteed to be up-to-date.
+  // The output of the updater is shallowly merged with prevState.
+  // toggleTheme = () => {
+  //   this.setState((prevState, props) => {
+  //     return { isLightTheme: !prevState.isLightTheme };
+  //   });
+  // };use this one
   render() {
     return (
-      <ThemeContext.Provider value={{ ...this.state }}>
+      <ThemeContext.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme }}
+      >
         {this.props.children}
       </ThemeContext.Provider>
     );
