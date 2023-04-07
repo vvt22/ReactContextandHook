f6a32a17f569d40537ace3afc0bb1639528cee4a
iam_Vaibhav <vaibhavthobbi93@gmail.com>
2023-04-07 20:54:20 +0530

    creatingcontextwithfuncomponent

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Navbar from "./components/Navbar";
 import ThemeContextProvider from "./contexts/ThemeContext";
 import ThemeToggle from "./components/ThemeToggle";
 import AuthContextProvider from "./contexts/AuthContext";
+import BookContextProvider from "./contexts/BookContext";
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
         <AuthContextProvider>
           <Navbar />
-          <BookList />
+          <BookContextProvider>
+            <BookList />
+          </BookContextProvider>
           <ThemeToggle />
         </AuthContextProvider>
       </ThemeContextProvider>
